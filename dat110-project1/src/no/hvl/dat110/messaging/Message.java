544bcc6bbb74347487e443446094fa32d3cdc3a9
kilka <kilka@DESKTOP-IJTAHA6>
2021-02-05 15:36:54 +0100

    Implementert Message klassen

--- dat110-project1/src/no/hvl/dat110/messaging/Message.java
+++ dat110-project1/src/no/hvl/dat110/messaging/Message.java
@@ -1,15 +1,14 @@
 package no.hvl.dat110.messaging;
 
-import java.util.Arrays;
-
-import no.hvl.dat110.TODO;
-
 public class Message {
 
 	private byte[] payload;
 
 	public Message(byte[] payload) {
-		this.payload = payload; // TODO: check for length within boundary
+		if (payload.length <= 127)
+			this.payload = payload;
+		else
+			this.payload = null;
 	}
 
 	public Message() {
@@ -17,31 +16,35 @@ public class Message {
 	}
 
 	public byte[] getData() {
-		return this.payload; 
+		return this.payload;
 	}
 
 	public byte[] encapsulate() {
-		
-		byte[] encoded = null;
-		
-		// TODO
+
+		byte[] encoded = new byte[128];
+
 		// encapulate/encode the payload of this message in the
 		// encoded byte array according to message format
-		
-		if (true)
-		   throw new UnsupportedOperationException(TODO.method());
+
+		encoded[0] = (byte) payload.length;
+
+		for (int i = 1; i <= payload.length; i++) {
+			encoded[i] = payload[i - 1];
+		}
 
 		return encoded;
-		
 	}
 
 	public void decapsulate(byte[] received) {
 
 		// TODO
-		// decapsulate the data contained in the received byte array and store it 
+		// decapsulate the data contained in the received byte array and store it
 		// in the payload of this message
+		payload = new byte[received[0]];
 		
-		throw new UnsupportedOperationException(TODO.method());
-		
+		for (int i = 0; i < received[0]; i++) {
+			this.payload[i] = received[i + 1];
+		}
+
 	}
 }
